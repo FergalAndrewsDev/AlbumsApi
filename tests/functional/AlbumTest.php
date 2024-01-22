a086a917cf374b2751da4946e1132b86e41057cc
Fergal Andrews <fandrews@rawnet.com>
2024-01-22 19:07:44 +0000

    Added extra functional tests (entity tests)

--- tests/functional/AlbumTest.php
+++ tests/functional/AlbumTest.php
@@ -37,27 +37,47 @@ class AlbumTest extends ApiTestCase
                 'hydra:next' => '/api/alba?page=2',
             ],
         ]);
-
         $this->assertCount(5, $response->toArray()['hydra:member']);
-
         $this->assertMatchesResourceCollectionJsonSchema(Album::class);
     }
 
     public function testCreateAlbum(): void
     {
         $response = static::createClient()->request('POST', '127.0.0.1:8000/api/alba', [
-                'headers' => [
-                    'Content-Type' => 'application/ld+json'
-                ],
-                'json' => [
-                    'name' => 'Revolver',
-                    'description' => 'A great album',
-                    'genre' => 'Rock/Pop',
-                    'artist' => '/api/artists/4',
-                ],
-            ]);
-
+            'headers' => [
+                'Content-Type' => 'application/ld+json'
+            ],
+            'json' => [
+                'name' => 'Revolver',
+                'description' => 'A great album',
+                'genre' => 'Rock/Pop',
+                'artist' => '/api/artists/4',
+            ],
+        ]);
         $this->assertResponseStatusCodeSame(201);
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
+    public function testPagination(): void
+    {
+        $client = static::createClient(['HTTP_HOST' => '127.0.0.1:8000']);
+        $response = $client->request('GET', '127.0.0.1:8000/api/alba?page=2');
+
+        $this->assertJsonContains([
+            '@context' => '/api/contexts/Album',
+            '@id' => '/api/alba',
+            '@type' => 'hydra:Collection',
+            'hydra:totalItems' => 100,
+            'hydra:view' => [
+                '@id' => '/api/alba?page=2',
+                '@type' => 'hydra:PartialCollectionView',
+                'hydra:first' => '/api/alba?page=1',
+                'hydra:last' => '/api/alba?page=20',
+                'hydra:previous' => '/api/alba?page=1',
+                'hydra:next' => '/api/alba?page=3',
+            ],
+        ]);
+        $this->assertCount(5, $response->toArray()['hydra:member']);
+
     }
 }
--- tests/functional/ArtistTest.php
+++ tests/functional/ArtistTest.php
@@ -43,16 +43,36 @@ class ArtistTest extends ApiTestCase
     public function testCreateArtist(): void
     {
         $response = static::createClient()->request('POST', '127.0.0.1:8000/api/artists', [
-                'headers' => [
-                    'Content-Type' => 'application/ld+json'
-                ],
-                'json' => [
-                    'name' => 'test_artist',
-                    'formedDate' => '1985-07-31T00:00:00+00:00',
-                ],
-            ]);
-
+            'headers' => [
+                'Content-Type' => 'application/ld+json'
+            ],
+            'json' => [
+                'name' => 'test_artist',
+                'formedDate' => '1985-07-31T00:00:00+00:00',
+            ],
+        ]);
         $this->assertResponseStatusCodeSame(201);
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
+    public function testPagination(): void
+    {
+        $client = static::createClient(['HTTP_HOST' => '127.0.0.1:8000']);
+        $response = $client->request('GET', '127.0.0.1:8000/api/artists?page=2');
+
+        $this->assertJsonContains([
+            '@context' => '/api/contexts/Artist',
+            '@id' => '/api/artists',
+            '@type' => 'hydra:Collection',
+            'hydra:totalItems' => 10,
+            'hydra:view' => [
+                '@id' => '/api/artists?page=2',
+                '@type' => 'hydra:PartialCollectionView',
+                'hydra:first' => '/api/artists?page=1',
+                'hydra:last' => '/api/artists?page=2',
+                'hydra:previous' => '/api/artists?page=1',
+            ],
+        ]);
+        $this->assertCount(5, $response->toArray()['hydra:member']);
     }
 }
